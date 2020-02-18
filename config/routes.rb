8f07f285af16e92c1a5d8f19751efb7c1e9e395a
satoshi.funayama <lala.un.azalea@gmail.com>
2020-02-18 01:36:06 +0000

    navbarにsignupとloginを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,7 @@
 Rails.application.routes.draw do
-    # #CURD
-    # get 'tasks/:id', to: 'tasks#show'
-    # post 'tasks', to: 'tasks#create'
-    # put 'tasks/:id', tyo: 'tasks#update'
-    # delete 'tasks/:id', to: 'tasks#destroy'
-    
-    # #index: show の補助ページ
-    # get 'tasks', to: 'tasks#index'
-    
-    # # new: 新規作成用のフォーページ
-    # get 'tasks/new', to: 'tasks#new'
-    
-    # # get: 更新用のフォームページ
-    # get 'tasks/:id:edit', to: 'tasks#edit'
-    
      root to: 'tasks#index'
+     
+     
      
      resources :tasks
 end
