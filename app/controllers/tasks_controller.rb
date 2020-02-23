40c9e32d229cfd1f11aa4b9ff7155984a88df67d
satoshi.funayama <lala.un.azalea@gmail.com>
2020-02-23 07:22:17 +0000

    レビューで指摘された点を修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,10 @@
 class TasksController < ApplicationController
-    before_action :require_user_logged_in, only: [:index]
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :require_user_logged_in
     before_action :correct_user, only: [:show, :edit, :update, :destroy]
     
     
     def index
-      if logged_in?
         @tasks = current_user.tasks.page(params[:page]).per(3)
-      else
-        redirect_to :new
-      end
     end
     
     def show
@@ -54,10 +49,6 @@ class TasksController < ApplicationController
     end
     
     private
-    
-    def set_task
-        @task = Task.find(params[:id])
-    end
     
     def task_params
         params.require(:task).permit(:content, :status)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,4 @@
 class UsersController < ApplicationController
-  
-  def index
-    #@users = User.order(id: :desc).page(params[:page]).per(25)
-  end
-
-  def show
-  end
 
   def new
     @user = User.new
